7a63555aa9ed06872514eaef3d8396dd95ea20e1
Christopher DeMarco <cdemarco@drwholdings.com>
2024-12-06 23:11:08 -0500

    Solve d02p02

--- day02/main.py
+++ day02/main.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
-# import os.path
-# print(os.path.curdir)
-# import sys; sys.exit()
+import sys
 
 def inc(a, b):
     return  0 < b - a < 4
@@ -28,12 +26,17 @@ def test_levels(b, levels):
 
 safe_levels = 0
 
-with open('input.dat', 'r') as ifh:
+with open(sys.argv[1], 'r') as ifh:
     for line in ifh.readlines():
         levels = line.rstrip().split()
 
-        b = int(levels.pop(0))
-        if test_levels(b, levels):
-            print(f'{line.rstrip()} is safe.')
-            safe_levels += 1
+        for i in range(len(levels)):
+            damped_levels = (levels[:i] + levels[i+1:])
+            b = int(damped_levels.pop(0))
+            if test_levels(b, damped_levels):
+                print(f'{line.rstrip()} is safe.')
+                safe_levels += 1
+                break
+            else:
+                continue
 print(safe_levels)
